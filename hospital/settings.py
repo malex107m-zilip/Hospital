81e5dde25de29ee73bf846099d7f4e8a9c91818a
mdmu107-arch <mdmu107@gmail.com>
2025-11-30 17:10:50 +0530

    Add Render URL to ALLOWED_HOSTS

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g9g-gcu)d=+07a2j4q1$-#$e2hdpzb(!4^aq#fmf2i_o_r@8b5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "hospital-pz7q.onrender.com"]
 
 
 # Application definition
